6f3bb74fe0ebb6d350844d27538a2a65cdfe638f
Полина <Shumilina246@gmail.com>
2022-06-24 12:28:51 +0300

    add: footer styles

--- src/shared/Footer/Footer.tsx
+++ src/shared/Footer/Footer.tsx
@@ -1,5 +1,9 @@
+import { url } from "inspector";
 import { useTranslation } from "react-i18next";
 import s from "./Footer.module.scss";
+import vk from "../../assets/svg/social/vk.svg";
+import instagram from "../../assets/svg/social/instagram.svg";
+import twitter from "../../assets/svg/social/twitter.svg";
 
 const Footer = () => {
   const { t } = useTranslation();
@@ -18,10 +22,48 @@ const Footer = () => {
               </span>
             </div>
             <span className={s.address}>
-              {t("footer.address_1")}
-              <br />
-              {t("footer.address_2")}
+              <a
+                href="https://yandex.ru/maps/-/CCUNE-uU2B"
+                target="_blank"
+                rel="noopener"
+              >
+                {t("footer.address_1")}
+                <br />
+                {t("footer.address_2")}
+              </a>
             </span>
+          </div>
+          <div className={s.social}>
+            <ul className={s.links}>
+              <li className={s.vk}>
+                <a
+                  href="https://vk.com/pushkeenru"
+                  target="_blank"
+                  rel="noopener"
+                >
+                  <img src={vk} alt="vkontakte" />
+                </a>
+              </li>
+              <li className={s.instagram}>
+                <a
+                  href="https://instagram.com/pushkeen.ru"
+                  target="_blank"
+                  rel="noopener"
+                >
+                  <img src={instagram} alt="instagram" />
+                </a>
+              </li>
+              <li className={s.twitter}>
+                <a
+                  href="https://twitter.com/pushkeen_nft?s=21&t=Xm7fVl18n8q-EBGiO1GX1g"
+                  target="_blank"
+                  rel="noopener"
+                >
+                  <img src={twitter} alt="twitter" />
+                </a>
+              </li>
+            </ul>
+            <span className={s.label}>PushKeen © 2021-2023</span>
           </div>
         </div>
       </div>
