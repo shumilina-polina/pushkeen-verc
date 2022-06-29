cc5ee979118f6b95d541f42f690c6d57c8c05334
Полина <Shumilina246@gmail.com>
2022-06-29 11:09:44 +0300

    add: nft & projects pages mobile

--- src/shared/Footer/Footer.tsx
+++ src/shared/Footer/Footer.tsx
@@ -12,7 +12,7 @@ const Footer = () => {
     <footer className={s.footer}>
       <div className="container">
         <div className={s.wrapper}>
-          <div className={s.info}>
+          <div className={s.social}>
             <div className={s.contacts}>
               <span className={s.email}>
                 <a href="mailto:hey@pushkeen.ru">hey@pushkeen.ru</a>
@@ -21,19 +21,6 @@ const Footer = () => {
                 <a href="tel:+78126794847">+7(812)679-48-47</a>
               </span>
             </div>
-            <span className={s.address}>
-              <a
-                href="https://yandex.ru/maps/-/CCUNE-uU2B"
-                target="_blank"
-                rel="noopener"
-              >
-                {t("footer.address_1")}
-                <br />
-                {t("footer.address_2")}
-              </a>
-            </span>
-          </div>
-          <div className={s.social}>
             <ul className={s.links}>
               <li className={s.vk}>
                 <a
@@ -63,6 +50,19 @@ const Footer = () => {
                 </a>
               </li>
             </ul>
+          </div>
+          <div className={s.info}>
+            <span className={s.address}>
+              <a
+                href="https://yandex.ru/maps/-/CCUNE-uU2B"
+                target="_blank"
+                rel="noopener"
+              >
+                {t("footer.address_1")}
+                <br />
+                {t("footer.address_2")}
+              </a>
+            </span>
             <span className={s.label}>PushKeen © 2021-2023</span>
           </div>
         </div>
