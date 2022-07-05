6c3725b9233a5579dea6bd1aeeb2a6e95e30a563
Полина <Shumilina246@gmail.com>
2022-07-05 13:05:35 +0300

    fix: ladoga

--- src/pages/Project/Ladoga/Ladoga.tsx
+++ src/pages/Project/Ladoga/Ladoga.tsx
@@ -35,7 +35,7 @@ export const Ladoga = () => {
       </div>
       <div className={s.buttons}>
         <a
-          href="https://play.google.com/store/apps/details?id=com.pushkeen.novayaladoga&hl=ru&gl=US"
+          href="https://apps.apple.com/ru/app/%D0%BD%D0%BE%D0%B2%D0%B0%D1%8F-%D0%BB%D0%B0%D0%B4%D0%BE%D0%B3%D0%B0/id1618416317"
           target="_blank"
           rel="noopener"
         >
