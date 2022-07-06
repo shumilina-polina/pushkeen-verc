16a3d970f02dedea797c4c4d071c9166d5db710d
Полина <Shumilina246@gmail.com>
2022-07-06 12:25:00 +0300

    create: public art page

--- src/pages/PublicArt/PublicArt.tsx
+++ src/pages/PublicArt/PublicArt.tsx
@@ -1,9 +1,13 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import { ButtonDownload } from "../../shared/components/ButtonDownload/ButtonDownload";
 import { Project } from "../Project/Project";
 import s from "./PublicArt.module.scss";
 import mainscreen from "../../assets/projects/publicart/mainscreen.png";
+// import image_1 from "../../assets/projects/publicart/image1.png";
+import image_2 from "../../assets/projects/publicart/image2.png";
+import image_3 from "../../assets/projects/publicart/image3.png";
+import image_4 from "../../assets/projects/publicart/image4.png";
 
 export const PublicArt = () => {
   const { t } = useTranslation();
@@ -29,7 +33,12 @@ export const PublicArt = () => {
               {t("project.list.publicart.about.content.name")}
             </h3>
             <span className={s.about_content_label}>
-              {t("project.list.publicart.about.content.label")}
+              {t("project.list.publicart.about.content.label_1")}
+            </span>
+            <br />
+            <br />
+            <span className={s.about_content_label}>
+              {t("project.list.publicart.about.content.label_2")}
             </span>
           </div>
         </div>
@@ -51,24 +60,67 @@ export const PublicArt = () => {
           <div className={s.app}>{t("project.list.publicart.buttons.3")}</div>
           <div className={s.game}>{t("project.list.publicart.buttons.4")}</div>
         </div>
-        {/* <img
-            className={s.image_1}
+        <div className={s.img_wrapper_1}>
+          {/* <img
+            className={s.image}
             src={image_1}
             alt="Public Art Screens"
           /> */}
-        <p className={s.desc}>
-          Сейчас Public ARt трансформировался просто из мобильного приложения в
-          полноценный бренд, который объединяет вокруг себя коммьюнити
-          художников и любителей искусства.
-        </p>
-        {/* <img
-            className={s.image_2}
-            src={image_2}
-            alt="Public Art"
-          /> */}
+          <div className={s.img_label}>
+            <span>{t("project.list.publicart.image_labels.app")}</span>
+            <div className={s.circle}></div>
+          </div>
+        </div>
+        <div className={s.desc}>
+          <h3 className={s.desc_title}>
+            {t("project.list.publicart.desc.title")}
+          </h3>
+          <p className={s.desc_text}>{t("project.list.publicart.desc.text")}</p>
+        </div>
+        <div className={s.img_wrapper_2}>
+          <img className={s.image} src={image_2} alt="Public Art" />
+          <div className={s.img_label}>
+            <span>{t("project.list.publicart.image_labels.street")}</span>
+            <div className={s.circle}></div>
+          </div>
+        </div>
         <div className={s.content}>
-          <div className={s.content_mission}></div>
-          <div className={s.content_direction}></div>
+          <div className={s.mission}>
+            <h3 className={s.mission_title}>
+              {t("project.list.publicart.content.mission.title")}
+            </h3>
+            <p className={s.mission_text}>
+              {t("project.list.publicart.content.mission.text_1")}
+              <br />
+              <br />
+              {t("project.list.publicart.content.mission.text_2")}
+            </p>
+          </div>
+          <div className={s.direction}>
+            <h3 className={s.direction_title}>
+              {t("project.list.publicart.content.direction.title")}
+            </h3>
+            <ul className={s.direction_text}>
+              <li>{t("project.list.publicart.content.direction.text_1")}</li>
+              <li>{t("project.list.publicart.content.direction.text_2")}</li>
+              <li>{t("project.list.publicart.content.direction.text_3")}</li>
+              <li>{t("project.list.publicart.content.direction.text_4")}</li>
+            </ul>
+          </div>
+        </div>
+        <div className={s.img_wrapper_3}>
+          <img className={s.image} src={image_3} alt="Public Art" />
+          <div className={s.img_label}>
+            <span>{t("project.list.publicart.image_labels.lenfilm")}</span>
+            <div className={s.circle}></div>
+          </div>
+        </div>
+        <div className={s.img_wrapper_4}>
+          <img className={s.image} src={image_4} alt="Public Art" />
+          <div className={s.img_label}>
+            <span>{t("project.list.publicart.image_labels.box")}</span>
+            <div className={s.circle}></div>
+          </div>
         </div>
       </section>
     </Project>
